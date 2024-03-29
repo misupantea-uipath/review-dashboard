9d33f00849321905cbab4511aba4b06df59d6f80
Cătălin Onuțu <catalin.onutu@uipath.com>
2024-03-29 16:08:23 +0200

    Refresh relative timestamps

--- src/pipes/time-ago.pipe.ts
+++ src/pipes/time-ago.pipe.ts
@@ -1,9 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import moment from 'moment';
+import { Observable, interval, map, startWith } from 'rxjs';
+
+const REFRESH_INTERVAL = 60 * 1000;
 
 @Pipe({ name: 'timeAgo', standalone: true })
 export class TimeAgoPipe implements PipeTransform {
-  transform(value: string | Date, withoutSuffix?: boolean): string {
-    return moment(value).fromNow(withoutSuffix);
+  transform(value: string | Date, withoutSuffix?: boolean): Observable<string> {
+    return interval(REFRESH_INTERVAL).pipe(
+      startWith(1),
+      map(() => moment(value).fromNow(withoutSuffix)),
+    );
   }
 }
